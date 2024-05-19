01c3d5bb2aeafaaf9aead28d37ed5cf25725b993
Tianyi.Huang <fh.frankhuang@gmail.com>
2024-05-19 11:44:34 +0100

    test API to find a movie by imdbId

--- src/main/java/frank/dev/movies/MovieService.java
+++ src/main/java/frank/dev/movies/MovieService.java
@@ -1,9 +1,11 @@
 package frank.dev.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
